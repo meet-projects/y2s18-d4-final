8d062abd78938b0371516092f713519f282fa714
aboud20-meet <aboud20@meet.mit.edu>
2018-08-15 20:38:03 +0300

    working on comments

--- databases.py
+++ databases.py
@@ -1,4 +1,4 @@
-from model import Base, Posts , User
+from model import Base, Posts , User , Comments
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
--- model.py
+++ model.py
@@ -39,3 +39,22 @@ class Posts(Base):
 					self.uname,
 					self.post,
 					self.username)
+
+class Comments(Base):
+	__tablename__='comments'
+	comment_id = Column(Integer , primary_key=True)
+	comment_body = Column(String)
+	commentor_name = Column(String)
+
+	def __repr__(self):
+		return ("comment_id {} \n"
+				"comment_body {} \n"
+				"commentor_name {} \n").format(
+					self.post_id,
+					self.comment_body,
+					self.commentor_name)
+
+
+
+
+			
